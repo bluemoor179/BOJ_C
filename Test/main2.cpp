edee3abc42d9ed8a2593cdbe9a8e0c94245e3610
bluemoor179 <bluemoor179@gmail.com>
2022-12-26 05:20:56 +0000

    first setting

--- Test/main2.cpp
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-int main(int argc, char* args[]) {
-    printf("aaaabbb\n");
-    return 0;
-}
---
+++ problem/1.Basic/1039.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(int argc, char* args[]) {
+    int n, k;
+    scanf("%d %d", &n, &k);
+    printf("%d %d\n", n, k);
+    return 0;
+}

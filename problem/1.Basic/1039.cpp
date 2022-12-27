86445209fbe305e565ed84850f8cc15c7e9a73c2
bluemoor179 <bluemoor179@gmail.com>
2022-12-27 07:01:18 +0000

    3. Data Structure

--- problem/1.Basic/1039.cpp
+++ problem/1.Basic/1039.cpp
@@ -1,8 +1,29 @@
 #include<stdio.h>
+#include<string.h>
 
 int main(int argc, char* args[]) {
-    int n, k;
-    scanf("%d %d", &n, &k);
-    printf("%d %d\n", n, k);
+    char n[1000001];
+    int k, m;
+    int count[10];
+
+    scanf("%s %d", n, &k);
+
+    for(int i = 0; i < 10; i++) {
+        count[i] = 0;
+    }
+    m = strlen(n);
+    for(int i = 0; i < m; i++) {
+        count[n[i] - '0']++;
+    }
+
+    int p = 0;
+    for(int i = 0; i < k; i++) {
+    }
+
+    printf("%s %d %d\n", n, m, k);
     return 0;
 }
+
+
+// 1. compare current n and max n without k.
+// 1.1 if current n is max n without k, calc max n with k (if k is even, no change, if k is odd, minimize diff.)
